a989ddd76ccb908d4c024ffab211bb7251899829
wkdrhkdwls <79461880+wkdrhkdwls@users.noreply.github.com>
2024-08-10 00:22:41 +0900

    Feat : camera setting

---
+++ src/pages/camera/index.tsx
@@ -0,0 +1,61 @@
+import * as mobilenet from '@tensorflow-models/mobilenet';
+import * as tf from '@tensorflow/tfjs';
+import { useEffect, useRef, useState } from 'react';
+
+const ImageClassifier: React.FC = () => {
+  const camera = useRef<HTMLVideoElement | null>(null);
+  const figures = useRef<HTMLDivElement | null>(null);
+  const [net, setNet] = useState<mobilenet.MobileNet | null>(null);
+
+  // Load the model on component mount
+  useEffect(() => {
+    const loadModel = async () => {
+      const loadedNet = await mobilenet.load();
+      setNet(loadedNet);
+    };
+
+    loadModel();
+  }, []);
+
+  // Initialize the webcam stream
+  useEffect(() => {
+    const setupCamera = async () => {
+      if (camera.current) {
+        try {
+          const stream = await navigator.mediaDevices.getUserMedia({
+            video: true,
+          });
+          camera.current.srcObject = stream;
+        } catch (err) {
+          console.error('Error accessing the webcam: ', err);
+        }
+      }
+    };
+
+    setupCamera();
+  }, [camera]);
+
+  const captureImage = async () => {
+    if (net && camera.current) {
+      const webcam = await tf.data.webcam(camera.current);
+      const img = await webcam.capture();
+      const result = await net.classify(img);
+
+      if (figures.current) {
+        figures.current.innerText = `Prediction: ${result[0].className}\nProbability: ${result[0].probability}`;
+      }
+
+      img.dispose();
+    }
+  };
+
+  return (
+    <>
+      <div ref={figures}></div>
+      <video autoPlay playsInline muted ref={camera} width="870" height="534" />
+      <button onClick={captureImage}>Capture Image</button>
+    </>
+  );
+};
+
+export default ImageClassifier;
--- src/router.tsx
+++ src/router.tsx
@@ -8,11 +8,13 @@ import Home from '@/pages/home';
 import { Toaster } from './components/ui/toaster';
 import Example from './pages/exmaple';
 import Example2 from './pages/example2';
+import ImageClassifier from './pages/camera';
 
 const pageRoutes = {
   main: '/',
   example: '/example',
   example2: '/example2',
+  camera: '/camera',
 };
 
 const CommonLayout = () => (
@@ -45,6 +47,14 @@ const router = createBrowserRouter([
     element: <CommonLayout />,
     children: [
       { path: pageRoutes.example2, element: <Example2 />, errorElement: <ErrorPage /> },
+
+      { path: '*', element: <NotFoundPage /> },
+    ],
+  },
+  {
+    element: <CommonLayout />,
+    children: [
+      { path: pageRoutes.camera, element: <ImageClassifier />, errorElement: <ErrorPage /> },
 
       { path: '*', element: <NotFoundPage /> },
     ],
