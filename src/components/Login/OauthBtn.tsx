81b7ee560f6db84eece806e3fda058bd458bc41c
ys0222 <ggdbstjd0222@gmail.com>
2024-08-10 16:30:40 +0900

    Feat : 버튼 추가

--- src/components/Login/OauthBtn.tsx
+++ src/components/Login/OauthBtn.tsx
@@ -2,13 +2,16 @@ interface OauthBtnProps {
   buttonText: string; // 버튼 텍스트
   iconUrl: string; // 아이콘 URL
   background: string; // 버튼 배경색
+  link : string; //오어스
+  handleLogin : (link : string) => void;
 }
 
-const OauthBtn = ({ buttonText, iconUrl, background }: OauthBtnProps) => {
+const OauthBtn = ({ buttonText, iconUrl, background,link, handleLogin }: OauthBtnProps) => {
   return (
     <button
       className={`w-[90%] flex items-center justify-center text-gray-800 font-semibold py-2 px-4 rounded hover:bg-opacity-80 active:bg-opacity-70 transition duration-300`}
       style={{ background: background }}
+      onClick={()=>handleLogin(link)}
     >
       <img
         className="mr-2"
