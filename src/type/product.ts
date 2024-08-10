5fb094537b503dc8bd0708313046fa4bf4d91ab4
wkdrhkdwls <79461880+wkdrhkdwls@users.noreply.github.com>
2024-08-10 22:04:05 +0900

    Refactor: Update Nutrition type properties

--- src/type/product.ts
+++ src/type/product.ts
@@ -1,9 +1,20 @@
+// export type Nutrition = {
+//   foodName: string; // 식품명
+//   nutritionStandardAmount: string; // 기준량
+
+//   manufacturerName: string; // 제조사
+//   energyKcal: number; // 칼로리
+//   foodWeight: string; // 중량
+//   carbohydrateG: number; // 탄수화물
+//   proteinG: number; // 단백질
+//   fatG: number; //지방
+// };
+
 export type Nutrition = {
-  foodName: string; // 식품명
-  manufacturerName: string; // 제조사
-  energyKcal: number; // 칼로리
-  foodWeight: string; // 중량
-  carbohydrateG: number; // 탄수화물
-  proteinG: number; // 단백질
-  fatG: number; //지방
+  foodName: string;
+  nutritionStandardAmount: string;
+  energyKcal: number;
+  carbohydrateG: number;
+  proteinG: number;
+  fatG: number;
 };
