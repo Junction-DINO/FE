90d6c4f7bc837e1d2fb9c7bb7839ad77091931ad
ys0222 <ggdbstjd0222@gmail.com>
2024-08-10 19:41:21 +0900

    Design : footer 구현

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,6 +1,6 @@
 import ImageSelectButton from '@/components/Camera/ImageButton';
 import Chatbot from '@/components/chatbot/ChatBox';
-// import { SearchInput } from '@/components/search/SearchInput';
+import { SearchInput } from '@/components/search/SearchInput';
 import Text from '@/components/common/Text';
 import { useState } from 'react';
 import Love from '@/assets/Home/Love.svg';
@@ -34,7 +34,7 @@ export default function Home() {
               {' '}
               {/* 첫 번째 행은 200px, 나머지는 유동적으로 설정 */}
               <GridDiv height="h-[200px]">
-                <p className="text-custionGrey">8th month ago</p>
+                <p className="text-customGrey">8th month ago</p>
                 <p className="">content</p>
               </GridDiv>
               <div className="grid grid-cols-3 gap-2 row-span-1 h-full">
@@ -45,7 +45,7 @@ export default function Home() {
                   {/* 가운데 아이템을 1.5 비율로 설정 */}
                 </GridDiv>
                 <GridDiv height="h-40">
-                  <p className="text-custionGrey">
+                  <p className="text-customGrey">
                     Last week’s <br />
                     score
                   </p>
@@ -53,12 +53,15 @@ export default function Home() {
               </div>
               <GridDiv height="h-40"></GridDiv>
             </div>
-
+            
             <Chatbot />
-            {/* <SearchInput /> */}
-            <ImageSelectButton />
+            
           </>
         </div>
+        <div className='fixed bottom-0 w-full max-w-[430px]'>
+            <SearchInput />
+            <ImageSelectButton />
+            </div>
       </main>
     </div>
   );
